d8fcc7a150ba16c708fc54347f83ed1361999418
Tommo216 <TSWAIN300@caledonian.ac.uk>
2024-10-23 20:43:23 +0100

    Basic Ship Damage and Filling System
    Extremely basic, ship floods at a speed related to how damaged the ship is. If filled 100% game over.

---
+++ Assets/Scripts/World/ShipHealth.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHealth : MonoBehaviour
+{
+    [SerializeField] float maxShipHealth;
+    [SerializeField] float fillRate;
+    [SerializeField] float shipFilled;
+    [SerializeField] float shipHealth;
+    [SerializeField] float percentageDamaged;
+    [SerializeField] float fillSpeed;
+    [SerializeField] GameManager gm;
+    bool fill=true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shipHealth = maxShipHealth;
+        shipFilled = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(percentageDamaged != 0 && fill)
+        {
+            StartCoroutine(fillShip());
+            fill = false;
+        }
+    }
+
+    public void DamageShip(int damage)
+    {
+        shipHealth = Mathf.Clamp(shipHealth - damage, 0, maxShipHealth);
+        percentageDamaged = Mathf.Lerp(shipHealth, 0, maxShipHealth) * 100;
+    }
+
+    public void repairShip(int repair)
+    {
+        shipHealth = Mathf.Clamp(shipHealth + repair, 0, maxShipHealth);
+        percentageDamaged = 100-(Mathf.Lerp(shipHealth, 0, maxShipHealth) * 100);
+    }
+
+    IEnumerator fillShip()
+    {
+        fillSpeed = percentageDamaged / fillRate;
+        shipFilled=shipFilled + Mathf.Clamp(fillSpeed,0,100);
+        if (shipFilled == 100)
+        {
+            gm.gameOver = true;
+        }
+        yield return new WaitForSeconds(0.5f);
+        fill = true;
+    }
+
+    public void emptyShip(float remove)
+    {
+        shipFilled = Mathf.Clamp(0,100,remove);
+    }
+}
