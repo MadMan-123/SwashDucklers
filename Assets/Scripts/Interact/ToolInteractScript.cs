310661ac3681c4f182ebdd518e4344cbace76a82
MadMan-123 <madocwolstencroft@gmail.com>
2024-10-27 01:23:10 +0100

    Minor bug fix with items and slapping

--- Assets/Scripts/Interact/ToolInteractScript.cs
+++ Assets/Scripts/Interact/ToolInteractScript.cs
@@ -10,7 +10,11 @@ public class ToolInteractScript : InteractArea
     //changed the system so it not just holds a function the on interact will call - MW
     public void SetAndRemoveItem(GameObject player,float speed)
     {
-        player.GetComponent<InteractComponent>().tool = myTool;
+        if (player.TryGetComponent(out InteractComponent comp))
+        {
+            comp.tool = myTool;
+            comp.inArea = false;
+        }
         //gameObject.SetActive(false); - Set false then destroy it? no need for this line if we destroy it MW
         Destroy(gameObject);
     }
