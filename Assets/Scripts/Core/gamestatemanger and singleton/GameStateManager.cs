642c9f3129becb0f020824426b130a85ae108e68
MyColdGrace <geraldthomasduffymccoygamedev@gmail.com>
2024-11-18 10:33:17 +0000

    game state manager work
    done work on setting up case breaks (place holder variables right now, will recontextualize later today & tomorrow)
    
    If I'm editing things like this I'll comment out the code so to limit risks of breaking the projec

--- Assets/Scripts/Core/gamestatemanger and singleton/GameStateManager.cs
+++ Assets/Scripts/Core/gamestatemanger and singleton/GameStateManager.cs
@@ -12,6 +12,34 @@ public class GameStateManager : Singleton<GameStateManager>  //GM: this will inh
     public static event Action<GameStateManager> OnBEFOREStateChanged;
     public static event Action<GameStateManager> OnAFTERStateChanged;
 
-   // public GameStateManager { get; private set; } GM: WILL COME BACK TO THIS BIT LATER
+    // public GameStateManager { get; private set; } GM: WILL COME BACK TO THIS BIT LATER
+
+    //GM: this will kick the first state...TIME TO BOOGALOO
+    //WILL COMMENTED OUT CODE TO I CAN EDIT IT SO IT DOESN'T MESS WITH PUSHES N PULLS
+   /* private void Start() -> changeState(GameStateManager newState)
+    {
+        State = newState;
+        switch (newState)
+        {
+               case GameStateManager.STARTING;
+                HandleSTARTING(); 
+                break;
+               case GameStateManager.SPAWNINGPlayers;
+                HandleSPAWNINGPlayers();
+                break;
+                case GameStateManager.SPAWNINGEnemies;
+                HandleSPAWNINGEnemies();
+                break;
+                case GameStateManager.KrakenATTACK;
+                HandleKrakenATTACK;
+                break;
+                case
+                 
+        }
+
+    }
+    */
+        
+    
 
 }
