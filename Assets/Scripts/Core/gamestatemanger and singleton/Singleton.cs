5ac027e99e82e1deaa75e90dbb78b98bde75f54e
GTDM_GAMEDEV <geraldthomasduffymccoygamedev@gmail.com>
2024-11-18 07:38:29 +0000

    updated the singleton
    did work on the singleton to progress with game state manager (might need to merge code with game manager at later point, but that's easily done)

--- Assets/Scripts/Core/gamestatemanger and singleton/Singleton.cs
+++ Assets/Scripts/Core/gamestatemanger and singleton/Singleton.cs
@@ -1,20 +1,45 @@
 using UnityEngine;
 
-public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour { 
+/// <summary>
+/// GM: this singleton allows us to have a single static instance of this class that can be easily accessed anywhere
+/// GM: code structure should be kept simple :D
+/// </summary>
 
-    public static T instance {  get; private set; }
-    protected virtual void Awake() => instance = this as T;
+public abstract class StaticInstance<T> : MonoBehaviour where T : MonoBehaviour 
+{
+  
 
-    protected virtual void onApplicationQuit()
-    {
-        instance = null;
-        Destroy(gameObject);
-    }
+        public static T instance { get; private set; }
+        protected virtual void Awake() => instance = this as T;
+
+        protected virtual void onApplicationQuit()
+        {
+            instance = null;
+            Destroy(gameObject);
+        }
+
 
     
 }
 
-public abstract class SingletonPersistent<T> : Singleton<T> where T : MonoBehaviour 
+/// <summary>
+/// GM: this stransforms the static instance into a basic singleton, this will destroy any
+/// new versions created, leaving the original isntance intact :D
+/// </summary>
+public abstract class Singleton<T> : StaticInstance<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        if (instance != null) Destroy(gameObject);
+        base.Awake();
+    }
+}
+
+/// <summary>
+/// GM: this singleton is more like the standard type of singleton as there is onle a single because we ensure there is onle ony version of the actual object
+/// GM: as well as there is a call for "dontdestroyonload(gameobject)"
+/// </summary>
+public abstract class SingletonPersistent<T> : Singleton<T> where T : MonoBehaviour //the the reason for the "persistent" is because this will persist throughout scenes
 {
     protected override void Awake()
     {
@@ -22,6 +47,5 @@ public abstract class SingletonPersistent<T> : Singleton<T> where T : MonoBehavi
             if (instance != null) Destroy(gameObject);
             DontDestroyOnLoad(gameObject); 
             base.Awake();
-        
     }
 }
