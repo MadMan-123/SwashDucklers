6186658728fdcf8b9528d6ff6ceeed8e3093a755
Tommo216 <TSWAIN300@caledonian.ac.uk>
2024-11-25 22:32:45 +0000

    Started Kraken AI, really cant focus
    will continue later (45 mins)

--- Assets/Scripts/AI/tentacleAI.cs
+++ Assets/Scripts/AI/tentacleAI.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class tentacleAI : MonoBehaviour
 {
+    public GameObject[] tentacleObjects;
+    int randomNumber;
+    public bool krakenAlive;
+    public float[] weightedSums;         //as many of these as locations
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomNumber = Random.Range(0, tentacleObjects.Length);        //start with a random number
+        for (int i = 0; i < weightedSums.Length; i++)
+        {
+            weightedSums[i] = Random.Range(0, 10);
+        }
+            PickTentacles(randomNumber);
+    }
+    private void FixedUpdate()
+    {
+        for(int i = 0; i < weightedSums.Length;i++)
+        {
+            weightedSums[i] += Time.deltaTime;
+        }
+    }
+    public void PickTentacles(int number)
+    {
+        float totalSum = 0;
+        for (int i = 0; i < weightedSums.Length; i++)
+        {
+            totalSum += weightedSums[i];                            //add them all together, random number using the sum as a cap. Depending on the number is what formation is used, might take top 3 and rand between them
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    IEnumerator TentacleTimer(GameObject[] tentacles, int time, int waitTime)
     {
-        
+        foreach(GameObject tentacle in tentacles)
+        {
+            //tentacle.setActive(true);       //or instantiate
+            //tentacle.anim.play              //lowkey forgot how to do animations plus they dont exist yet
+        }
+        yield return new WaitForSeconds(time);
+
+        foreach(GameObject tentacle in tentacles)
+        {
+            //delete / set inactive
+        }
+        yield return new WaitForSeconds(waitTime);
     }
 }
+//weighted sums
