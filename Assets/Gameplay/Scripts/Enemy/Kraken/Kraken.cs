5cb39935f7c002385c2f63c92479348fc4ae9078
Tommo216 <TSWAIN300@caledonian.ac.uk>
2025-01-31 17:47:26 +0000

    Kraken eye follows first duck
    Will change after talk with designers and game manager. Struggling to lock the rotation past a certain spot.
    (1hour 15)

--- Assets/Gameplay/Scripts/Enemy/Kraken/Kraken.cs
+++ Assets/Gameplay/Scripts/Enemy/Kraken/Kraken.cs
@@ -6,21 +6,30 @@ using UnityEngine.Experimental.GlobalIllumination;
 public class Kraken : MonoBehaviour
 {
     //This script is for controlling the kraken body and animations of the cosmetic tentacles
+    //Wait til all intro anims are done before moving eye
 
 
     [SerializeField] GameObject eye;
     [SerializeField] GameObject eyeTarget;
     [SerializeField] PlayerManager pm;
+    [SerializeField] Vector3 playerLoc;
+    [SerializeField] bool activate;
 
     void Start()
     {
-
+        activate = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(pm.players.Count > 0 && activate==true)
+        {
+            eyeTarget = pm.players[0];
+            StartCoroutine(EyeFollow());
+            activate = false;
+            Debug.Log("First call");
+        }
 
     }
 
@@ -34,23 +43,23 @@ public class Kraken : MonoBehaviour
         //NormalWater();
     }
 
-    public void EyeFollow()
+    public IEnumerator EyeFollow()
     {
-        for (int i = 0;i < pm.players.Count;i++)
+        eye.transform.LookAt(eyeTarget.transform.position);
+        eye.transform.rotation *= Quaternion.FromToRotation(Vector3.left, Vector3.forward);
+        if (eye.transform.rotation.x > 30)
         {
-            if(pm.players[i].TryGetComponent(out Health hp))
-            {
-                var temp = hp.GetHealth();
-               if (temp > eyeTarget.GetComponent<Health>().GetHealth())
-                {
-                    eyeTarget = pm.players[i];
-                }
-            }
+                        Debug.Log("clamp");
+
+            //eye.transform.eulerAngles = new Vector3(30f,eye.transform.eulerAngles.y,eye.transform.eulerAngles.z);
+            eye.transform.Rotate(30f, eye.transform.eulerAngles.y, eye.transform.eulerAngles.z);
         }
-        transform.LookAt(eyeTarget.transform.position);
-        //var lookPos = eyeTarget.transform.position - transform.position;
-        //var rotation = Quaternion.LookRotation(lookPos);
-        //transform.rotation = Quaternion.Slerp(transform.rotation,rotation,Time.deltaTime * damping);
-        EyeFollow();
+        else if (eye.transform.rotation.x < -30)
+        {
+            Debug.Log("clamp");
+            eye.transform.Rotate(-30f, eye.transform.eulerAngles.y, eye.transform.eulerAngles.z);
+        }
+            yield return new WaitForSeconds(0.05f);
+        StartCoroutine(EyeFollow());
     }
 }
