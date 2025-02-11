23e14555cbe9f101ecaf38a142a781fe07c33c37
Tommo2160 <TSWAIN300@caledonian.ac.uk>
2025-02-11 11:45:08 +0000

    Start of sprite animations for UI ship
    2D ui amination started

---
+++ Assets/TestS.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestS : MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();
+        anim.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
