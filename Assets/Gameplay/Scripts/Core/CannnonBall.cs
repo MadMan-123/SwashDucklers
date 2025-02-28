3bb8002b5bff95159d42681fa81f82b3e666387b
Tommo216 <TSWAIN300@caledonian.ac.uk>
2025-02-28 16:07:41 +0000

    Added VFX for cannonballs hitting kraken
    OMG this took so long, i think i forgot how to code for part of it but ill just say like (45mins)

--- Assets/Gameplay/Scripts/Core/CannnonBall.cs
+++ Assets/Gameplay/Scripts/Core/CannnonBall.cs
@@ -6,14 +6,25 @@ using UnityEngine;
 public class CannnonBall : MonoBehaviour
 {
     public float damage = 1;
+    [SerializeField]private GameObject impactAnim;
+    private Transform cam;
+
+    private void Start()
+    {
+        cam = Camera.main?.gameObject.transform;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.TryGetComponent(out Health hp))
         {
+            Vector3 lookDir = (cam.transform.position - gameObject.transform.position).normalized;
+            Quaternion direction = Quaternion.LookRotation(lookDir);
+            Instantiate(impactAnim,gameObject.transform.position,direction);
             hp.TakeDamage(gameObject, damage);
             //return to the pool
             gameObject.SetActive(false);
-            
+
         }
     }
 }
---
+++ Assets/Gameplay/Scripts/Enemy/Kraken/KrakenImpact.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KrakenImpact : MonoBehaviour
+{
+  [SerializeField]private GameObject impactAnim;
+  private Transform cam;
+
+  private void Start()
+  {
+    cam = Camera.main?.gameObject.transform;
+  }
+
+  private void OnTriggerEnter(Collider col)
+  {
+    if (col.gameObject.TryGetComponent<CannnonBall>(out var ball))
+    {
+      Vector3 lookDir = cam.transform.position - col.gameObject.transform.position;
+      Quaternion direction = Quaternion.LookRotation(lookDir);
+      Instantiate(impactAnim,col.gameObject.transform.position,direction);
+    }
+    Debug.Log("Impacted");
+    
+  }
+}
+
