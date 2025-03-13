17445a3d327731c8b5ca972cab7432dcc4716a41
Tommo2160 <TSWAIN300@caledonian.ac.uk>
2025-03-13 14:35:54 +0000

    Made a script and did research 💀

---
+++ Assets/Gameplay/Scripts/Interact/General/InteractFlash.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractFlash : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == 2)
+        {
+            
+        }
+    }
+}
